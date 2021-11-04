816ae75f25c39bd9089618c884b01504365d93b4
Mikhail Fedyakov <mf_52@mail.ru>
2021-11-04 16:24:18 +0300

    improved push_back and reserve

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -9,6 +9,13 @@ TEST(basic_operation, constructor_empty) {
     Vector<int> v;
     ASSERT_NE(&v, nullptr);
 }
+//
+//TEST(basic_operation, constructor_non_empty) {
+//    std::size_t f = 5;
+//    int s = 2;
+//    Vector<int> v{f, s};
+//    ASSERT_NE(&v, nullptr);
+//}
 
 TEST(basic_operation, move_constructor) {
     Vector<int> v1;
@@ -62,7 +69,6 @@ TEST(push_back, push_back_fold_expr) {
 TEST(push_back, push_back_fold_expr_realloc) {
     Vector<int> v1;
     v1.push_back(1,2,3,4,5,6,7,8,9);
-    std::cout << v1;
     ASSERT_EQ(v1.len(), 9);
     ASSERT_EQ(v1.cap(), 16);
     ASSERT_EQ(v1[0], 1);
@@ -81,13 +87,39 @@ TEST(push_front, push_front) {
     ASSERT_EQ(v1[3],1);
     ASSERT_EQ(v1[4],2);
     ASSERT_EQ(v1[5],3);
-    std::cout << v1;
+    ASSERT_EQ(v1.len(),6);
+}
+
+TEST(pop_back, pop_back) {
+    Vector<int> v1;
+    v1.push_back(1,2,3);
+    v1.push_front(10,20,30);
+    ASSERT_EQ(v1[0],10);
+    ASSERT_EQ(v1[1],20);
+    ASSERT_EQ(v1[2],30);
+    ASSERT_EQ(v1[3],1);
+    ASSERT_EQ(v1[4],2);
+    v1.pop_back();
+    ASSERT_EQ(v1.len(),5);
+}
+
+TEST(pop_front, pop_front) {
+    Vector<int> v1;
+    v1.push_back(1,2,3);
+    v1.push_front(10,20,30);
+    v1.pop_front();
+    ASSERT_EQ(v1.len(),5);
+    ASSERT_EQ(v1[0],20);
+    ASSERT_EQ(v1[1],30);
+    ASSERT_EQ(v1[2],1);
+    ASSERT_EQ(v1[3],2);
+    ASSERT_EQ(v1[4],3);
 }
 
 
 
-//int main() {
-//    testing::InitGoogleTest();
-//    return RUN_ALL_TESTS();
-//}
+int main() {
+    testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
 
--- vector/include/vector.h
+++ vector/include/vector.h
@@ -12,19 +12,45 @@
 #define dbg std::cout << __PRETTY_FUNCTION__ << '\n';
 namespace no_std {
 
-    template<class T>
+    template<class T, typename Alloc = std::allocator<T>>
     class Vector {
     public:
-        Vector():
-            data_ptr(new T[4]) {
+        Vector(const T& new_entry = T(), const Alloc& alloc = Alloc()):
+                    capacity(default_capacity),
+                    allocator(alloc) {
+//            reserve(capacity);
+            data_ptr = allocator.allocate(capacity);
+            for (int i = 0; i < capacity; i++) {
+                new(data_ptr + i) T(new_entry);
+            }
         }
 
-        Vector(u32 capacity):
-            data_ptr(new T[capacity]),
-            capacity(capacity) {
+        Vector(std::size_t count,
+               const T& value,
+               const Alloc& alloc = Alloc()): allocator(alloc) {
+            reserve(count);
+            for (std::size_t i = 0; i < count; i++) {
+                alloc.construct(data_ptr + i, value);
+            }
         }
 
-        Vector(Vector<T>&& old) {
+        explicit Vector(u32 capacity,
+                        const T& new_entry = T(),
+                        const Alloc& alloc = Alloc()):
+            capacity(capacity),
+            allocator(alloc)
+        {
+            data_ptr = allocator.allocate(capacity);
+            for (int i = 0; i < capacity; i++) {
+                new(data_ptr + i) T(new_entry);
+            }
+        }
+
+        Vector(Vector<T>&& old)  noexcept {
+            if (&old == data_ptr) {
+                return;
+            }
+            delete[] data_ptr;
             data_ptr = old.data_ptr;
             size = old.size;
             capacity = old.capacity;
@@ -32,15 +58,11 @@ namespace no_std {
             old.size = 0;
             old.capacity = 0;
         }
-
         Vector(const Vector<T>& old) {
             if (&old == this) {
                 return;
             }
-            if (data_ptr) {
-                delete[] data_ptr;
-                data_ptr = nullptr;
-            }
+            delete[] data_ptr;
             data_ptr = new T[old.size];
             size = old.size;
             capacity = old.capacity;
@@ -48,30 +70,26 @@ namespace no_std {
                 data_ptr[i] = old.data_ptr[i];
             }
         }
-
-        Vector<T>& operator=(Vector<T>&& old) {
+        Vector<T>& operator=(Vector<T>&& old)  noexcept {
             if (&old == this) {
                 return *this;
             }
-            if (data_ptr) {
-                delete[] data_ptr;
-                capacity = 0;
-                size = 0;
-            }
+            delete[] data_ptr;
+            data_ptr = nullptr;
+            capacity = 0;
+            size = 0;
+
             std::exchange(data_ptr, old.data_ptr);
             std::exchange(size, old.size);
             std::exchange(capacity, old.capacity);
             return *this;
         }
-
         Vector<T>& operator=(const Vector<T>& old) {
             if (&old == this) {
                 return *this;
             }
-            if (data_ptr) {
-                delete[] data_ptr;
-                data_ptr = nullptr;
-            }
+            delete[] data_ptr;
+
             data_ptr = new T[old.size];
             size = old.size;
             for (unsigned i = 0; i < size; i++) {
@@ -79,15 +97,12 @@ namespace no_std {
             }
             return *this;
         }
-
         ~Vector() {
             delete[] data_ptr;
         }
-
         T* operator&() {
             return data_ptr;
         }
-
         T& operator*() {
             return *data_ptr;
         }
@@ -107,7 +122,7 @@ namespace no_std {
             ring_shift(shift);
         }
 
-        void ring_shift(u32 shift) {
+        void ring_shift(std::size_t shift) {
             for (int i = size-1; i >= 0; i--) {
                 data_ptr[i + shift] = data_ptr[i];
             }
@@ -116,29 +131,70 @@ namespace no_std {
             }
         }
 
-        void push_front_impl(const T& new_entry) {
-            std::cout << new_entry << "\n";
-        }
-
-        void shift_right(u32 shift) {
-            for (int i = size - 1; i >= 0; i--) {
-                std::exchange(data_ptr[i + shift], 0);
-            }
-        }
-
         //todo: if move noexcept then move othervice copy and clear old storage
         // or i can create subarray and copy it in front of data_ptr...
-        constexpr void reserve(u32 new_capacity) noexcept {
+        // std::addressof(*iter) вызывается в uni copy потому что он может не иметь перегруженного оператора *
+        constexpr void reserve(std::size_t new_capacity) noexcept {
             if (capacity >= new_capacity) {
                 return;
             }
-            capacity = new_capacity;
-            T *new_storage = new T[capacity];
-            for (int i = 0; i < size; i++) {
-                new_storage[i] = data_ptr[i];
+            std::size_t i = 0;
+            T* new_storage = allocator.allocate(new_capacity);
+
+//            T *new_storage = reinterpret_cast<T*>(new char[ new_capacity * sizeof(T)]); // create new array with [n bytes X size of T] //   T *new_storage = new T[capacity];
+
+            try {
+                std::uninitialized_copy(data_ptr, data_ptr + size, new_storage);
+            } catch (...) {
+                allocator.deallocate(new_storage, new_capacity);
+//                delete[] reinterpret_cast<char*>(data_ptr);
+                throw;
             }
-            delete[] data_ptr;
+            //try {
+            //    for (; i < size; i++) {
+            //         По идее плейсмент нью вызывает констр по адресу, не выделяя память.
+            //        new(new_storage + i) T(data_ptr[i]); //placement new syntax // Означает, что нужно по адресу [new_storage + i] положить такой объект [data_ptr[i]] // new_storage[i] = data_ptr[i]; Так лучше не делать, т.к. это подразумевает, что объект слева уже существует и будет вызван его деструктор. Это будет UB.
+            //    }
+            //} catch (...) { // Защищаемся от эксепшена при копировании
+            //    for (std::size_t j = 0; j < i; j++) {
+            //        (new_storage + i)->~T();
+            //    }
+            //    delete[] reinterpret_cast<char*>(data_ptr);
+            //    throw;
+            //}
+            for (std::size_t i = 0; i < size; i++) {// delete[] data_ptr;  Тоже UB. Вызывает деструктор объекта, который мог быть не проинициализирован.
+//                data_ptr[i].~T();
+                allocator.destroy(data_ptr + i);
+            }
+
+            allocator.deallocate(data_ptr, size);
+//            delete[] reinterpret_cast<char*>(data_ptr); // Надо удалить выделенную память как char, чтобы он не вызывал деструкторы, а просто вызвал очищение.
             data_ptr = new_storage;
+            capacity = new_capacity;
+        }
+
+        // second param is to create base object. But it works only for objects with default constructor
+        //void resize(u32 new_size, const T& value = T()) {
+        // this is bad case, cus when we do reserve we construct a lot of objects... there might be a problem
+        // Внутри addressof делает reinterpred_cast<T*>(static_cast<char&>(reinterpted_cast<const volatile char&>())
+        void resize(u32 new_size, const T& value = T()) {
+            if (new_size > size) {
+                reserve(new_size);
+                std::size_t i = 0;
+                try {
+                    for (; i < size; i++) {
+                        new(data_ptr + i) T(data_ptr[i]); //placement new syntax // Означает, что нужно по адресу [new_storage + i] положить такой объект [data_ptr[i]] // new_storage[i] = data_ptr[i]; Так лучше не делать, т.к. это подразумевает, что объект слева уже существует и будет вызван его деструктор. Это будет UB.
+                    }
+                } catch (...) { // Защищаемся от эксепшена при копировании
+                    for (std::size_t j = 0; j < i; j++) {
+                        (data_ptr + i)->~T();
+                    }
+                    delete[] reinterpret_cast<char*>(data_ptr);
+                    throw;
+                }
+            } else if (new_size < size) {
+                size -= new_size;
+            }
         }
 
         // todo: prob need to use noexcept for []noexcept when << and [] with exceptions for deref operation.
@@ -147,7 +203,7 @@ namespace no_std {
             os << "\tsize = " << v.size;
             os << "\tcapacity = " << v.capacity;
             os << "\tdata = [ ";
-            for (int i = 0; i < v.size; i++) {
+            for (std::size_t i = 0; i < v.size; i++) {
                 os << v.data_ptr[i] << " ";
             }
             os << "]\n";
@@ -156,6 +212,17 @@ namespace no_std {
 
         // todo: prob need to throw exception to avoid data access after ring shifting
         T& operator[](u32 pos) noexcept {
+            return data_ptr[pos];
+        }
+        // todo: prob need to throw exception to avoid data access after ring shifting
+        const T& operator[](u32 pos) const noexcept {
+            return data_ptr[pos];
+        }
+
+        T& at(u32 pos) {
+            if (pos >= size) {
+                throw std::out_of_range("");
+            }
             return data_ptr[pos];
         }
 
@@ -167,34 +234,88 @@ namespace no_std {
             return capacity;
         }
 
-        void pop_back() {
-
+        constexpr void pop_back() noexcept {
+            (data_ptr + size)->~T();
+            size -= 1;
         }
 
-        void pop_front() {
-            
+        constexpr void pop_front(const T& value = T()) {
+            for (std::size_t i = 0; i < size - 1; i++) {
+                data_ptr[i] = data_ptr[i + 1];
+            }
+            size -= 1;
+            data_ptr[size] = value;
         }
+
+        //void srink_to_fit();
 
     protected:
+        // базовая гарантия - если кто-то бросил исключение, то контейнер останется в валидном состоянии
+        // строгая - контейнер в неизменном состоянии, если кто-то бросил исключение
+        // в деструкторе не надо кидать исключения ни при каких условиях
+        // Если в конструкторе копирования вывалось исключение, то мы идём в цикле по всем скопированным объектам
+        // и вызываем их деструкторы.
         void push_back_impl(const T& new_entry) {
             if (size == capacity) {
-                capacity *= capacity_multiplier;
-                T* new_storage = new T[capacity];
-                for (int i = 0; i < size; i++) {
-                    new_storage[i] = data_ptr[i];
-                }
-                delete[] data_ptr;
-                data_ptr = new_storage;
+                reserve(capacity * capacity_multiplier);
             }
-            data_ptr[size] = new_entry;
-            size += 1;
+
+            std::size_t i = size;
+            try {
+                new(data_ptr + size) T(new_entry);
+                size += 1;
+//                for (; i < size; i++) {
+//                    new(data_ptr + size) T(new_entry); //placement new syntax // Означает, что нужно по адресу [new_storage + i] положить такой объект [data_ptr[i]] // new_storage[i] = data_ptr[i]; Так лучше не делать, т.к. это подразумевает, что объект слева уже существует и будет вызван его деструктор. Это будет UB.
+//                }
+            } catch (...) { // Защищаемся от эксепшена при копировании
+                size -= 1;
+                for (std::size_t j = 0; j < i; j++) {
+                    (data_ptr + i)->~T();
+                }
+                delete[] reinterpret_cast<char*>(data_ptr);
+                throw;
+            }
         }
 
     protected:
         T* data_ptr {nullptr};
-        u32 size {0};
-        u32 capacity {4};
+        std::size_t size {0};
+        std::size_t capacity {4};
+
+        Alloc allocator;
 
         static constexpr f32 capacity_multiplier {2.0}; // 2.0 std, fibonaccy for apple
+        static constexpr std::size_t default_capacity {4}; // 2.0 std, fibonaccy for apple
     };
+
+    // Вектор из чаров будет занимать 1 байт на клетку, вектор булей будет занимать 4 элемента на байт
+    // operator[] у vector<bool> возвращает bit_reference
+    /*template<>
+    class Vector<bool> {
+    private:
+        uint8_t* arr;
+        struct BitReference {
+            uint8_t* bucket_ptr;
+            uint8_t pos;
+
+            BitReference operator=(bool b) {
+                if (b) {
+                    *bucket_ptr |= ((uint8_t)1 << pos);
+                } else {
+                    *bucket_ptr &= ~((uint8_t)1 << pos);
+                }
+                return *this;
+            }
+
+            explicit operator bool() const {
+                return *bucket_ptr & (1 << pos);
+            }
+        };
+    public:
+        BitReference operator[](std::size_t i) {
+            uint8_t pos = i % 8;
+            uint8_t* ptr = arr + i / 8;
+            return BitReference(ptr, pos);
+        }
+    };*/
 }
